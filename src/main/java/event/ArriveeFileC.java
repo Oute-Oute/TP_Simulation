6c9428896c8dacfce25a1740af3a9f246ed156b0
Aurélie THURIOT <aurelie.thuriot@etu.univ-tours.fr>
2022-11-29 11:04:29 +0100

    ArriveeFileC fini

--- src/main/java/event/ArriveeFileC.java
+++ src/main/java/event/ArriveeFileC.java
@@ -4,22 +4,22 @@ import main.java.Controller;
 import main.java.Event;
 import main.java.Scheduler;
 
-public class ArrFc extends Event {
+public class ArriveeFileC extends Event {
 
     /**
      * Instantiates a new main.java.Event.
      *
      * @param startingTime the starting time
      */
-    public ArrFc(float startingTime) {
+    public ArriveeFileC(float startingTime) {
         super(startingTime);
     }
 
     @Override
     public void run() {
-        Controller.getControllerInstance().setControlQueueArea(Controller.getControllerInstance().getControlQueueArea() + 1);
+        Controller.getControllerInstance().getPosteControle().setQueue(Controller.getControllerInstance().getPosteControle().getQueue() + 1);
         if (Controller.getControllerInstance().getPosteControle().isFree()){
-            Scheduler.getInstance().addEvent(new AccesGc(Scheduler.getInstance().getCurrentTime()));
+            Scheduler.getInstance().addEvent(new AccesControle(Scheduler.getInstance().getCurrentTime()));
         }
     }
 }
