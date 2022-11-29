a16d93bb666598d57b96ddb9fe9f5a55ff8bcdc6
Auraen <aurane.martin@etu.univ-tours.fr>
2022-11-29 11:46:01 +0100

    feat: DepartReparation

---
+++ src/main/java/event/DepartReparation.java
@@ -0,0 +1,24 @@
+package main.java.event;
+
+import main.java.Controller;
+import main.java.Event;
+import main.java.Scheduler;
+
+public class DepartReparation extends Event {
+
+    public DepartReparation(double startingTime) {
+        super(startingTime);
+    }
+
+    @Override
+    public void run() {
+        if(!Controller.getInstance().getPostesReparations().get(0).isFree()) {
+            Controller.getInstance().getPostesReparations().get(0).setIsFree(true);
+        } else {
+            Controller.getInstance().getPostesReparations().get(1).setIsFree(true);
+        }
+        if(Controller.getInstance().getQueueR() > 0){
+            Scheduler.getInstance().addEvent(new AccesReparation(Scheduler.getInstance().getCurrentTime()));
+        }
+    }
+}
